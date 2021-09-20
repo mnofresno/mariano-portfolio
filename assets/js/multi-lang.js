d58d7537931d47ec9633a2eff1cde32e08f2035a
Mariano Fresno <mnofresno@gmail.com>
2021-09-20 00:24:33 -0300

    Add services to home page

--- assets/js/multi-lang.js
+++ assets/js/multi-lang.js
@@ -7,7 +7,20 @@
             aboutSubtitle: "Technical Leader & Web Developer.",
             aboutSubtext: "With experience in the formation of teams and the permanent training of personnel, I work empathetically to achieve the objectives set by the business without forgetting that software development is a human activity where attention to detail also contributes to quality.",
             skillsSubtitle: "Here is a summary list of some of my competencies and the level at which I currently perform at each of those.",
-            socialFooter: "You can contact me through all these social networks or using the contact information in the 'About' section."
+            socialFooter: "You can contact me through all these social networks or using the contact information in the 'About' section.",
+            servicesSubtitle: "If you need any of these services please feel free to contact me so we can achieve together a solution that suits your needs:",
+            serviceTechTeams: "Technical Teams",
+            serviceTechTeamsDesc: "Technical evaluation of IT profiles, work management with teams formed ad-hoc by project.",
+            serviceDev: "Software Development",
+            serviceDevDesc: "I build Web applications and also command line tools, desktop systems. Multi-architecture, multi-pattern designs.",
+            servicePerformance: "Optimization",
+            servicePerformanceDesc: "Database performance optimization, time profile debugging, mitigation of N + 1 queries, Caches implementation, infrastructure solutions.",
+            serviceAnalysis: "Analysis and QA",
+            serviceAnalysisDesc: "Functional analysis of use cases of existing and projected systems, generation of documentation and estimates. Coverage analysis and continuous quality improvement.",
+            serviceTraining: "Trainings",
+            serviceTrainingDesc: "Team mentoring, tailor-made training, code review, content generation for technical learning.",
+            serviceHosting: "Web Hosting",
+            serviceHostingDesc: "Automated deployment of CI/CD, cloud services for delivered projects and pre-existing systems, domain and SSL certificates management."
         },
         es: {
             iam: "Soy",
@@ -16,7 +29,20 @@
             aboutSubtitle: "Líder Técnico y Desarrollador Web",
             aboutSubtext: "Con experiencia en la conformación de equipos y la capacitación permanente del personal, trabajo empáticamente para alcanzar los objetivos planteados por el negocio sin olvidar que el desarrollo de software es una actividad humana donde la atención al detalle también aporta calidad.",
             skillsSubtitle: "Aquí se encuentra una lista resumida de algunas de mis competencias y el nivel con el que actualmente me desempeño en cada una.",
-            socialFooter: "Puede comunicarse conmigo a través de todas éstas redes sociales o utilizando los datos de contacto de la sección 'About'."
+            socialFooter: "Puede comunicarse conmigo a través de todas éstas redes sociales o utilizando los datos de contacto de la sección 'About'.",
+            servicesSubtitle: "Si necesita alguno de estos servicios por favor contácteme para que podamos lograr juntos una solución que se acomode a sus necesidades:",
+            serviceTechTeams: "Equipos Técnicos",
+            serviceTechTeamsDesc: "Evaluación técnica de perfiles IT, gestión de trabajos con equipos conformados ad-hoc por proyecto.",
+            serviceDev: "Desarrollo de Software",
+            serviceDevDesc: "Construyo aplicaciones Web y también herramientas de línea de comando, sistemas de escritorio, diseños multi-arquitectura, multi-patrón.",
+            servicePerformance: "Optimización",
+            servicePerformanceDesc: "Optimización de performance en bases de datos, depuración de perfiles de tiempos, mitigación de N+1 queries, implementación de Cachés, soluciones de infraestructura.",
+            serviceAnalysis: "Análisis y Control de Calidad",
+            serviceAnalysisDesc: "Análisis funcional de casos de uso nuevos y existentes, generación de documentación y estimaciones. Análisis de cobertura y mejora continua de la calidad.",
+            serviceTraining: "Capacitaciones",
+            serviceTrainingDesc: "Mentoreo de equipos, capacitaciones a medida, revisión de código, generación de contenidos para aprendizajes técnicos.",
+            serviceHosting: "Web Hosting",
+            serviceHostingDesc: "Despliegue automatizado CI/CD, cloud services para proyectos propios y sistemas pre-existentes, gestión de dominios y certificados SSL."
         }
     };
 
